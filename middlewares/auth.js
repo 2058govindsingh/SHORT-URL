d739ce38e24166bdf0abdc137cc216428276e791
Govind Singh <govindyadav2058@gmail.com>
2023-11-20 19:35:46 +0530

    Resolved too many calls to route

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,25 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedInUserOnly(req, res, next) {
-
-    const userUid = req.headers["authorization"]
-    if(!userUid) return res.redirect("/login");
-    const token = userUid.split('Bearer ')[1]
-    console.log(token);
-    const user = await getUser(token);
-    if(!user) return res.redirect("/login");
-    console.log("aagye");
-    req.user = user;
-    next();
+  const userUid = req.headers["authorization"];
+  if (!userUid) return res.redirect("/login");
+  const token = userUid.split("Bearer ")[1];
+  const user = await getUser(token);
+  if (!user) return res.redirect("/login");
+  req.user = user;
+  next();
 }
 async function checkAuth(req, res, next) {
-
-    const userUid = req.cookies?.uid;
-    const user = await getUser(userUid);
+  const userUid = req.headers["authorization"];
+  const token = userUid.split("Bearer ")[1];
+  const user = await getUser(token);
+  if (user) {
     req.user = user;
-    next();
+  }
+  next();
 }
 
 module.exports = {
-    restrictToLoggedInUserOnly,
-    checkAuth,
-}
+  restrictToLoggedInUserOnly,
+  checkAuth,
+};
